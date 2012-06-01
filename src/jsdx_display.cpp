1dda87bd00c8904478e3dcb432361e3e93ba1a65
Fred Chien <fred@mandice.com>
2012-06-02 05:20:45 +0800

    getting screen size with Xlib API

--- src/jsdx_display.cpp
+++ src/jsdx_display.cpp
@@ -12,11 +12,37 @@ namespace JSDXDisplay {
 	using namespace node;
 	using namespace v8;
 
+	static Handle<Value> GetScreenWidth(const Arguments& args)
+	{
+		HandleScope scope;
+		XWindowAttributes windowattr;
+
+		/* Get current display and screen */
+		Display *disp = XOpenDisplay(NULL);
+		int screen = DefaultScreen(disp);
+		XGetWindowAttributes(disp, RootWindow(disp, screen), &windowattr);
+
+		return scope.Close(Integer::New(windowattr.width));
+	}
+
+	static Handle<Value> GetScreenHeight(const Arguments& args)
+	{
+		HandleScope scope;
+		XWindowAttributes windowattr;
+
+		/* Get current display and screen */
+		Display *disp = XOpenDisplay(NULL);
+		int screen = DefaultScreen(disp);
+		XGetWindowAttributes(disp, RootWindow(disp, screen), &windowattr);
+
+		return scope.Close(Integer::New(windowattr.height));
+	}
+
 	static void init(Handle<Object> target) {
 		HandleScope scope;
 
-//		NODE_SET_METHOD(target, "x11", ClutterInit);
-//		ADCAWrapper::Initialize(target);
+		NODE_SET_METHOD(target, "getScreenWidth", GetScreenWidth);
+		NODE_SET_METHOD(target, "getScreenHeight", GetScreenHeight);
 	}
 
 	NODE_MODULE(jsdx_display, init);
